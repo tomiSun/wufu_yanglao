008b76b47a3f00c22c633374ef042455e81a24eb
USER-20210502FS\Administrator <1838382763@qq.com>
2021-05-17 03:36:43 +0800

    init

---
+++ src/pages/laorendangan/hetong/index.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+import { Button, Card, Col, Form, List, Row, Select, Tag } from 'antd';
+
+
+const A = (props) => {
+    return (
+        <div>合同</div>
+    )
+};
+
+export default A
